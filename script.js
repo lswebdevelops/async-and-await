b9b20463d7a09e549df68aaac359471d90e22688
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-02-01 20:51:59 -0300

    use an api to get avatars from gitnub using async and await

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-const container = document.querySelector('.containerOne');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const img = document.createElement('img');
-container.appendChild(img);
-img.classList.add('container');
+async function showAvatar() {
 
+  // read our JSON
+  let response = await fetch('/article/promise-chaining/user.json');
+  let user = await response.json();
 
-button.addEventListener('click', () => {
-  const searchTerm = input.value;
-  fetchGif(searchTerm);
-  }); 
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user.login}`);
+  let githubUser = await githubResponse.json();
 
-async function fetchGif(searchTerm) {
- 
- try {
-   const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=${searchTerm}`,
-   {mode: 'cors'})
-    .then(response => response.json())
-    .then(response => {
-      img.src = response.data.images.original.url;
-    })
-  }
-  catch(error){
-    console.log("error: ", error);
-  };
+  // show the avatar
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+
+  img.remove();
+
+  return githubUser;
 }
 
-
+showAvatar();
